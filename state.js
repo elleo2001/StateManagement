308c49ed46334aa91bc9acc4c774525251285f22
elleo2001 <leonardosantana28@yahoo.com.br>
2022-12-29 20:10:02 -0300

    first commit

---
+++ state.js
@@ -0,0 +1,36 @@
+export const states = {
+    STANDING_LEFT: 0,
+    STANDING_RIGHT: 1,
+}
+
+class State {
+    constructor(state){
+        this.state = state;
+    }
+}
+
+class StandingLeft extends State {
+    constructor(player){
+        super('STANDING LEFT');
+        this.player = player;
+    }
+    enter(){
+
+    }
+    handleInput(input){
+
+    }
+}
+
+class StandingRight extends State {
+    constructor(player){
+        super('STANDING RIGHT');
+        this.player = player;
+    }
+    enter(){
+
+    }
+    handleInput(input){
+
+    }
+} 
